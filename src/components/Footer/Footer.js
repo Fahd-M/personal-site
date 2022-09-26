d00e0ef8aedcd3643cbc95b9e8a47656e64fed88
Fahd Munir <fahd.munir.89@gmail.com>
2022-09-26 16:38:05 -0400

    deleted server entirely to restart the structure

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link, NavLink } from "react-router-dom";
+import "./Footer.scss";
+
+import { slide as Menu } from 'react-burger-menu';
+
+function Footer() {
+  return (
+    <div>Footer</div>
+//     <nav class="navbar">
+//       <div class="navbar-container container">
+//           <input type="checkbox" name="" id=""/>
+//           <div class="hamburger-lines">
+//               <span class="line line1"></span>
+//               <span class="line line2"></span>
+//               <span class="line line3"></span>
+//           </div>
+//           <ul class="menu-items">
+//               <li><a href="#home">Home</a></li>
+//               <li><a href="#about">About</a></li>
+//               <li><a href="#food">Experience</a></li>
+//               <li><a href="#food-menu">Education</a></li>
+//               <li><a href="#contact">Contact</a></li>
+//           </ul>
+//           <h1 class="logo">FM</h1>
+//       </div>
+//   </nav>
+  )
+}
+
+export default Footer
