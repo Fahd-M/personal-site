3a285c655b88da6a0d34e8997d3072c8fcbbc54a
Fahd Munir <fahd.munir.89@gmail.com>
2022-10-03 19:17:31 -0400

    work experience slidedown added

--- src/pages/Experience/WorkExp.js
+++ src/pages/Experience/WorkExp.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import './WorkExp.scss';
-
+import { SlideDown } from 'react-slidedown';
+import 'react-slidedown/lib/slidedown.css';
+import { BsArrowRightCircle } from 'react-icons/bs';
 export const WorkExp = ({company, titleDate, description, pic}) => {
     const [isOpen, toggleBlock] = useState(false)
    
     return (
+    
+    <>
     <div className='work__wrapper' onClick={()=> toggleBlock(!isOpen)}>
         <div className='work__top'>
             <div className='work__text'>
@@ -15,7 +19,24 @@ export const WorkExp = ({company, titleDate, description, pic}) => {
                 <img src={pic} alt={company} className='work__img'/>
             </div>
         </div>
+        <SlideDown className={'my-dropdown-slidedown'}>
+            {isOpen ? 
+                <ul className='work__description'>
+                    {description.map(line => 
+                         <li className='work__line' key={line}>
+                            <BsArrowRightCircle
+                                color='white'
+                                size={40}
+                                className='work__icon'/>
+                             {line}
+                        </li>    
+                    )}
+                </ul> : null 
+            
+            }
+        </SlideDown>
     </div>
+    </>
     // <div className='dropdownslidedown here'>
 
     // </div>
