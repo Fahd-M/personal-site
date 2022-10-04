eb90dfd72e995e689d677cebf2c75d0416170b19
Fahd Munir <fahd.munir.89@gmail.com>
2022-10-03 23:32:58 -0400

    education created but needs animation style fix

--- src/pages/Education/Education.js
+++ src/pages/Education/Education.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Education.scss';
+import { SlideDown } from 'react-slidedown';
+import 'react-slidedown/lib/slidedown.css';
+import eduMessages from './eduMessages';
+import { Schools } from './Schools';
+
 
 function Education() {
   return (
-    <div>Education</div>
+    <div className='education__wrapper'>
+      <section className='section education'>
+        <h1 className='education__title'>Education</h1>
+        <div className='education__info'>
+          <h2 className='education__school'>School</h2>
+          {eduMessages.degrees.map(item => <Schools key={item.name} {...item}/>)}
+        </div>
+      </section>
+    </div>
   )
 }
 
